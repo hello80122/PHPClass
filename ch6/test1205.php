cee3e8c88152151aac8bb8c2998663d9b8c0af15
hello80122 <hello80122@gmail.com>
2019-12-05 20:59:04 +0800

    add 1205 new file

---
+++ ch6/test1205.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>php 1205 test</title>
+</head>
+<body>
+    <?php 
+    print("hello world")
+    ?>
+</body>
+</html>
