23632e49666ba85a1a3670b1db7d9422d390e9ff
to84510 <to84510@gmail.com>
2019-12-12 20:28:23 +0800

    ch6_15.php

--- ch6/ch6_15.php
+++ ch6/ch6_15.php
@@ -1,10 +1,11 @@
 <?php
     while($i <= 10){
+        $i++;
       if ( $i == 5) {
         echo "略過 \$i = $i <br />";
         continue;
       }
       echo $i."<br />";
-    $i++;
+    
 }
   ?>
