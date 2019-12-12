237b887f9e4e5cd2435d8bab69e83bbb48033157
to84510 <to84510@gmail.com>
2019-12-12 20:03:52 +0800

    ch6_12.php

--- ch6/ch6_12.php
+++ ch6/ch6_12.php
@@ -1,13 +1,15 @@
 <?php
-  $fruit = "Apple";
-  switch($fruit){
-    case "apple":
-      echo "這是 apple";
-      break;
-    case "banana":
-      echo "這是 banana";
-      break;
-    default:
-      echo "打錯字了嗎";
+  // 從 1 加到 50 的整數和
+  // 以下方式只是為了說明 for 迴圈的使用方式，不是好的程式碼寫法！！
+  $sum = 20;
+  for ($i = 1; $i <= 50; i++){
+      $sum += $i;
+  }  
+  echo "總合是: $sum";
+
+  $sum = 10;
+  while ( $i <= 50 ){
+    $sum += $i;
+    $i++;
   }
 ?>
