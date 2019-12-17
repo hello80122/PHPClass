e61521e9f4bda5114264d3ad21a3e4016d936a43
to84510 <to84510@gmail.com>
2019-12-17 19:39:53 +0800

    MyClass.php

---
+++ ch7/MyClass.php
@@ -0,0 +1,19 @@
+<?php
+    class MyClass {
+      public $x = 123;
+      protected $y = "Spring";
+      private $z = 'hello';
+
+      public function getPara(){
+        echo "inclass->x : ".$this->x."<br />";
+        echo "inclass->y : ".$this->y."<br />";
+        echo "inclass->z : ".$this->z."<br />";
+      }
+    }
+
+    $test = new MyClass();
+    $test->getPara();
+    echo "test->x : ".$test->x."<br />";
+    echo "test->y : ".$test->y."<br />";
+    echo "test->z : ".$test->z."<br />";
+  ?>
