718b49478871601f4c373a75e494f6dc88f78039
to84510 <to84510@gmail.com>
2019-12-19 21:19:03 +0800

    s

--- ch7/Person.php
+++ ch7/Person.php
@@ -1,15 +1,14 @@
 <?php
-<<<<<<< HEAD
   class Person {
       private $sex;
       public function __set($name, $value){
         
         //限制不可動態產生屬性
-        if (isset($this->$name)) {
+       /* if (isset($this->$name)) {
             return $this->$name = $value;
         } else {
             return null;
-        }
+        } */
       }
       // 取得屬性名稱的值
       public function __get($name){
@@ -34,34 +33,4 @@
     echo $person->sex.PHP_EOL;
     echo isset($person->address);
     unset($person->name);
-=======
-class Person {
-    private $sex;
-    public function __set($name,$value){
-
-        if(isset($this->$name)) {
-            return $this->name = $value;
-        }
-        else{
-            return null;
-        }
-    }
-    public function __get($name){
-        return $name;
-    }
-    public function __isset($name){
-        return $name;
-    }
-    public function __unset($name){
-        return $name;
-    
-$person = new Person();
-$person->name = 'PHP';
-echo $person -> name.PHP_EOL;
-echo $person -> sex.PHP_EOL;
-echo isset($person -> address);
-unset($person->name);
-    }
-}
->>>>>>> eab61a9901cf9d2c0eb16f393a074b301427692f
 ?>
