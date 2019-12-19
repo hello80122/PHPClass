c7d1e524e353f61c294948ca69905dc96c70b0ba
to84510 <to84510@gmail.com>
2019-12-19 21:31:47 +0800

    s

---
+++ ch7/Shape.php
@@ -0,0 +1,23 @@
+<?php
+class Shape {
+    const PI = 3.142;
+    function __call($name,$arg){
+        if($name == 'area')
+        switch(count($arg))
+        {
+            case 0 : return 0;
+            case 1 : return self::PI * $arg[0];
+            case 2 : return $arg[0] * $arg[1];
+        }
+    }
+    function __callStatic($name,$arg){
+        return array(3,5);
+    }
+}
+
+$circle = new Shape();
+echo $circle -> area(3);
+$rect = new Shape();
+echo $rect -> area(8,6);
+echo Sharp::hello()[1];
+?>
