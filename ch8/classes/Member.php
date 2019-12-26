b77fa2ab05dede76d9616d23c7d0070a5a53cb0b
to84510 <to84510@gmail.com>
2019-12-26 20:00:50 +0800

    a

--- ch8/classes/Member.php
+++ ch8/classes/Member.php
@@ -1,8 +1,7 @@
 <?php
-//這是 Classes/Member.php
 class Member{
-  public function getMemberList(){
-    echo "print member list...<br>";
-  }
+    public function getMemberList(){
+        echo "piint member list...<br>";
+    }
 }
 ?>
--- ch8/index.php
+++ ch8/index.php
@@ -1,7 +1,6 @@
 <?php
-//這是 index.php
 include_once __DIR__ . "/autoload.php";
 
 $member = new Member();
-$member->getMemberList();
+$member -> getMemberList();
 ?>
